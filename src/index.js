1f1b648aa78389c43373862a67adbd43e98c0454
HoannnLee <Leecghoan@gmail.com>
2025-05-13 10:31:04 +0700

    format code prettier

--- src/index.js
+++ src/index.js
@@ -6,10 +6,13 @@ const { engine } = require('express-handlebars');
 const app = express();
 const port = 3000;
 
-const route = require('./routes')
+const route = require('./routes');
+const db = require('./config/db');
+
+db.connect()
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // HTTP logger
@@ -21,8 +24,8 @@ app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources', 'views'));
 
 // Route
-route(app)
+route(app);
 
 app.listen(port, () => {
-  console.log(`Server chạy tại http://localhost:${port}`);
+    console.log(`Server chạy tại http://localhost:${port}`);
 });
