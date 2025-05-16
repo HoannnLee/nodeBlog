812f3e02daf5a97f6ce5580a083b953e9d4c12a0
HoannnLee <Leecghoan@gmail.com>
2025-05-16 11:49:49 +0700

    CRUD course

--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,14 +1,20 @@
 const mongoose = require('mongoose');
+const slug = require('mongoose-slug-generator');
 const Schema = mongoose.Schema;
 
+mongoose.plugin(slug);
+
 const Course = new Schema({
-    name: {type: String},
+    name: {type: String , required : true},
     description: {type: String},
     image: {type: String},
     countPeople: {type: Number},
     videoQuantity: {type: Number},
-    createdAt: {type: Date , default: Date.now},
-    updatetedAt: {type: Date , default: Date.now}
+    videoId: {type: String , required : true},
+    level: {type: String},
+    slug: { type: String, slug: 'name' , unique : true}
+},{
+    timestamps: true,
 });
 
 module.exports = mongoose.model('Course' , Course)
--- src/index.js
+++ src/index.js
@@ -2,15 +2,19 @@ const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const { engine } = require('express-handlebars');
+const methodOverride = require('method-override');
 
-const app = express();
-const port = 3000;
+
 
 const route = require('./routes');
 const db = require('./config/db');
 
+const app = express();
+const port = 3000;
+
 db.connect()
 
+app.use(methodOverride('_method'))
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -19,9 +23,15 @@ app.use(express.json());
 app.use(morgan('combined'));
 
 // Template engine
-app.engine('hbs', engine({ extname: '.hbs' }));
+app.engine('hbs', engine({
+     extname: '.hbs',
+     helpers: {
+        sum: (a,b) => a + b
+     }
+     }));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources', 'views'));
+
 
 // Route
 route(app);
